07e1c54673047f2943164b5fa51b2daa62b127eb
jcjones94 <jcjones9488@gmail.com>
2017-07-05 23:30:27 -0400

    Add files via upload

---
+++ trans.ino
@@ -0,0 +1,34 @@
+
+
+
+#include <RH_ASK.h>
+#include <SPI.h>
+
+
+RH_ASK driver;
+int power = 0;
+uint8_t data[2];
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  if(!driver.init())
+      Serial.println("init failed");
+  
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  data[0] = analogRead(A0)>>2;
+  data[1] = analogRead(A1)>>2;
+  data[2] = analogRead(A2)>>2;
+  driver.send(data, 3);
+  driver.waitPacketSent();
+  delay(3);
+  digitalWrite(13,LOW);
+  delay(5);
+}
