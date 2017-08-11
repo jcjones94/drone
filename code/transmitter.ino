b1221132a9383f8f827864625e1866ee4ecb7f99
jcjones94 <jcjones9488@gmail.com>
2017-08-11 18:35:07 -0400

    Update transmitter.ino

--- code/transmitter.ino
+++ code/transmitter.ino
@@ -20,12 +20,10 @@ void setup() {
 
 void loop() {
   digitalWrite(13, HIGH);
-  data[0] = analogRead(A0)>>2;
+  data[0] = analogRead(A0)>>2;     //shifting right by 2 converts it from a 10 bit value to byte for transmitting the data
   data[1] = analogRead(A1)>>2;
   data[2] = analogRead(A2)>>2;
-  driver.send(data, 3);
-  driver.waitPacketSent();
-  delay(3);
-  digitalWrite(13,LOW);
-  delay(5);
+  driver.send(data, 3);             //sends byte array of length 3
+  driver.waitPacketSent();          //waits for the packet to be send           
+ 
 }
